49fd20c57a0b7a18bb75b0882d0251bb7e59eb54
Caner Kuncu <warlordsw@gmail.com>
2021-05-17 23:24:38 +0300

    add Switch and Route to the react project for the new pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import FirebaseHome from './pages/firebaseHome'
+import MongoDBHome from './pages/mongodbHome'
+import LocalStorage from './pages/localStorage'
+import { Route, Switch } from 'react-router'
 import Home from './pages/home'
 
 function App() {
   return (
     <>
       <div className='container mx-auto '>
-        <Home />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/firebase'>
+            <FirebaseHome />
+          </Route>
+          <Route exact path='/mongodb'>
+            <MongoDBHome />
+          </Route>
+          <Route exact path='/localstorage'>
+            <LocalStorage />
+          </Route>
+        </Switch>
       </div>
     </>
   )
