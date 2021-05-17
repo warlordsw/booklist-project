c08e32bdf1be81af9e196027aaf7f7c08e0913df
Caner Kuncu <warlordsw@gmail.com>
2021-05-17 23:29:06 +0300

    add 3 new pages

---
+++ src/pages/localStorage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LocalStoragePc from '../components/Localstoragepc'
+
+const LocalStorage = () => {
+  return (
+    <div>
+      <LocalStoragePc />
+    </div>
+  )
+}
+
+export default LocalStorage
