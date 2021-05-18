d17eedeb82426d9ba2b24cb971555993e7e3f4ec
Caner Kuncu <warlordsw@gmail.com>
2021-05-18 22:50:10 +0300

    add cors and fix app.get as app.use

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import bookRoutes from './routes/book.js'
+import cors from 'cors'
 const app = express()
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
 
-app.get('/books', bookRoutes)
+app.use(express.json({ extended: false }))
+
+app.use('/books', bookRoutes)
 
 app.listen(5000, () => {
   console.log('Listening port on 5000...')
