fcb98e171c123483eb84ea42e28bcef898335fc1
Caner Kuncu <warlordsw@gmail.com>
2021-05-18 22:59:00 +0300

    add new routes

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,8 +1,16 @@
 import express from 'express'
-import { createBook } from '../controller/book_controller.js'
+import {
+  createBook,
+  getAllBooks,
+  removeSpecificBook,
+  removeAll,
+} from '../controller/book_controller.js'
 
 const router = express.Router()
 
-router.get('/', createBook)
+router.get('/localfile', getAllBooks)
+router.post('/localfile', createBook)
+router.post('/localfile/removespecific', removeSpecificBook)
+router.post('/localfile/removeall', removeAll)
 
 export default router
