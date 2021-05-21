0146843ae798491164cf1cbfda4184082d4852a3
Caner Kuncu <warlordsw@gmail.com>
2021-05-21 15:36:22 +0300

    modify firebase methods and change id=bookName to id=id

--- frontend/src/pages/firebaseHome.js
+++ frontend/src/pages/firebaseHome.js
@@ -2,12 +2,6 @@ import React, { useState, useEffect, useContext } from 'react'
 import BookList from '../components/BookList'
 import CreateBook from '../components/CreateBook'
 import FirebaseContext from '../context/firebase'
-import {
-  getBookList,
-  sendDatabase,
-  removeAllBooks,
-  removeSpecificBook,
-} from '../functions/firebase.js'
 
 const FireBaseHome = () => {
   //useContext hook
@@ -20,6 +14,41 @@ const FireBaseHome = () => {
   const [writerName, setWriterName] = useState('')
   const [pageNumber, setPageNumber] = useState('')
   const [info, setInfo] = useState('')
+  let id
+  // Function for get all document data from Firestore database and map it.
+  const getBookList = () => {
+    const getDatabase = async () => {
+      const snapshot = await bookData.firebase
+        .firestore()
+        .collection('books')
+        .get()
+      const result = await snapshot.docs.map((item) => item.data())
+      setLoading(false)
+      setList(result)
+    }
+    return getDatabase()
+  }
+
+  // Function for delete all books from Firestore database documents.
+  const removeAllBooks = async () => {
+    const db = await bookData.firebase.firestore()
+    const getId = await db.collection('books').get()
+    const result = await getId.docs.map((item) => item.id)
+    for (let i = 0; i < result.length; i++) {
+      await db.collection('books').doc(`${result[i]}`).delete()
+    }
+    return await getBookList()
+  }
+
+  //Function for send data to Firestore when onSubmit. It is working in handleSubmit method
+  async function sendDatabase(firebase) {
+    await firebase.firestore().collection('books').doc(id).set({
+      id: id,
+      bookName: bookName,
+      writerName: writerName,
+      pageNumber: pageNumber,
+    })
+  }
 
   //Function for submit button
   const handleSubmit = (e) => {
@@ -29,8 +58,9 @@ const FireBaseHome = () => {
     if (!bookName || !writerName || !pageNumber) {
       setInfo('Please fill the blanks')
     } else {
+      id = new Date().getTime().toString()
       const newBook = {
-        id: new Date().getTime().toString(),
+        id: id,
         title: bookName,
         writer: writerName,
         pageCount: pageNumber,
@@ -41,14 +71,24 @@ const FireBaseHome = () => {
       setWriterName('')
       setPageNumber('')
       setInfo('Book created')
-      sendDatabase(bookData.firebase, bookName, writerName, pageNumber)
-      getBookList(setLoading, setList, bookData)
+      sendDatabase(bookData.firebase)
+      getBookList()
     }
+  }
+
+  // Function for removing specific book from Firestore database.
+  const removeSpecificBook = async (id) => {
+    await bookData.firebase
+      .firestore()
+      .collection('books')
+      .doc(`${id}`)
+      .delete()
+    setList(list.filter((book) => book.id !== id))
   }
 
   // Render hook - [] means only working on initial state.
   useEffect(() => {
-    getBookList(setLoading, setList, bookData)
+    getBookList()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
@@ -68,11 +108,9 @@ const FireBaseHome = () => {
         <h1 className='text-6xl text-center mt-10 text-white'>Loading...</h1>
       ) : (
         <BookList
-          bookData={bookData}
-          setList={setList}
           items={list}
           removeSpecificBook={removeSpecificBook}
-          removeAllBooks={() => removeAllBooks(bookData, setLoading, setList)}
+          removeAllBooks={removeAllBooks}
         />
       )}
     </div>
