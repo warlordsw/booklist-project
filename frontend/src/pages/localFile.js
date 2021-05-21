a383072a87022f68e96d5bf49a8bee8a5dababcc
Caner Kuncu <warlordsw@gmail.com>
2021-05-21 15:37:25 +0300

    add booklist and createBook components to the localfile and localstorage pages

--- frontend/src/pages/localFile.js
+++ frontend/src/pages/localFile.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import CreateBook from '../components/CreateBook'
+import BookList from '../components/BookList'
 
 const LocalFile = () => {
   const [list, setList] = useState([])
-
   const [bookName, setBookName] = useState('')
   const [writerName, setWriterName] = useState('')
   const [pageNumber, setPageNumber] = useState('')
   const [info, setInfo] = useState('')
+  let id
 
   //Function for submit button
   const handleSubmit = (e) => {
@@ -17,11 +19,12 @@ const LocalFile = () => {
     if (!bookName || !writerName || !pageNumber) {
       setInfo('Please fill the blanks')
     } else {
+      id = new Date().getTime().toString()
       const newBook = {
-        id: new Date().getTime().toString(),
-        title: bookName,
-        writer: writerName,
-        pageCount: pageNumber,
+        id: id,
+        bookName: bookName,
+        writerName: writerName,
+        pageNumber: pageNumber,
       }
 
       setList([...list, newBook])
@@ -55,104 +58,21 @@ const LocalFile = () => {
 
   return (
     <div>
-      <div>
-        <nav className='bg-blue-500  text-center text-white px-6 py-3  mx-2'>
-          Create Book
-        </nav>
-      </div>
-      <div className='bg-red-200 mx-auto w-96 rounded-lg flex justify-center mt-20'>
-        <div>
-          <div className='p-3 text-center'>
-            <h6>Enter Book Name</h6>
-            <input
-              value={bookName}
-              onChange={(e) => setBookName(e.target.value)}
-              className='rounded-md'
-              type='text'
-              placeholder='Book Name'
-            />
-          </div>
-          <div className='p-3 text-center'>
-            <h6>Enter Writer Name</h6>
-            <input
-              value={writerName}
-              onChange={(e) => setWriterName(e.target.value)}
-              className='rounded-md'
-              type='text'
-              placeholder='Writer Name'
-            />
-          </div>
-          <div className='p-3 text-center'>
-            <h6>Enter Total Page Number </h6>
-            <input
-              value={pageNumber}
-              onChange={(e) => setPageNumber(e.target.value)}
-              className='rounded-md'
-              type='number'
-              placeholder='Page Number'
-            />
-          </div>
-          <div className='flex justify-center p-3'>
-            <form onSubmit={handleSubmit}>
-              <button className='bg-blue-500 py-3 px-6 rounded-full text-white'>
-                Submit
-              </button>
-            </form>
-          </div>
-          <div className='p-3 text-center  text-white'>
-            <h3>{info}</h3>
-          </div>
-        </div>
-      </div>
-      <div>
-        <Booklist items={list} removeSpecificBook={removeSpecificBook} />
-      </div>
-      {list.length > 1 && (
-        <div className='flex justify-center my-5'>
-          <button
-            onClick={removeAllBooks}
-            className='px-8 py-4 bg-red-500 rounded-full text-white'
-          >
-            Remove All
-          </button>
-        </div>
-      )}
-    </div>
-  )
-}
-
-// Function for rendering book list
-const Booklist = ({ items, removeSpecificBook }) => {
-  return (
-    <div className='container mx-auto'>
-      <div className='mt-20 flex flex-wrap items-center justify-center'>
-        {items.map((item) => {
-          return (
-            <div key={item.id} className='p-2 m-2 bg-yellow-100 w-1/4'>
-              <div className='p-1'>
-                <h5 className='font-semibold'>Book Name</h5>
-                <h3>{item.title}</h3>
-              </div>
-              <div className='p-1'>
-                <h5 className='font-semibold'>Writer Name</h5>
-                <h3>{item.writer}</h3>
-              </div>
-              <div className='p-1'>
-                <h5 className='font-semibold'>Total Page</h5>
-                <h3>{item.pageCount}</h3>
-              </div>
-              <div className='flex justify-end'>
-                <button
-                  onClick={() => removeSpecificBook(item.id)}
-                  className='px-4 py-2 bg-red-500 rounded-full text-white'
-                >
-                  Remove
-                </button>
-              </div>
-            </div>
-          )
-        })}
-      </div>
+      <CreateBook
+        bookName={bookName}
+        writerName={writerName}
+        pageNumber={pageNumber}
+        handleSubmit={handleSubmit}
+        info={info}
+        setBookName={setBookName}
+        setWriterName={setWriterName}
+        setPageNumber={setPageNumber}
+      />
+      <BookList
+        items={list}
+        removeSpecificBook={removeSpecificBook}
+        removeAllBooks={removeAllBooks}
+      />
     </div>
   )
 }
--- frontend/src/pages/localStorage.js
+++ frontend/src/pages/localStorage.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import BookList from '../components/BookList'
+import CreateBook from '../components/CreateBook'
 
 const getLocalStorage = () => {
   let list = localStorage.getItem('liste')
@@ -12,11 +14,11 @@ const getLocalStorage = () => {
 const LocalStorage = () => {
   //state hooks
   const [list, setList] = useState(getLocalStorage)
-
   const [bookName, setBookName] = useState('')
   const [writerName, setWriterName] = useState('')
   const [pageNumber, setPageNumber] = useState('')
   const [info, setInfo] = useState('')
+  let id
 
   //Function for submit button
   const handleSubmit = (e) => {
@@ -26,11 +28,12 @@ const LocalStorage = () => {
     if (!bookName || !writerName || !pageNumber) {
       setInfo('Please fill the blanks')
     } else {
+      id = new Date().getTime().toString()
       const newBook = {
-        id: new Date().getTime().toString(),
-        title: bookName,
-        writer: writerName,
-        pageCount: pageNumber,
+        id: id,
+        bookName: bookName,
+        writerName: writerName,
+        pageNumber: pageNumber,
       }
       setList([...list, newBook])
 
@@ -58,104 +61,22 @@ const LocalStorage = () => {
 
   return (
     <div>
-      <div>
-        <nav className='bg-blue-500  text-center text-white px-6 py-3  mx-2'>
-          Create Book
-        </nav>
-      </div>
-      <div className='bg-red-200 mx-auto w-96 rounded-lg flex justify-center mt-20'>
-        <div>
-          <div className='p-3 text-center'>
-            <h6>Enter Book Name</h6>
-            <input
-              value={bookName}
-              onChange={(e) => setBookName(e.target.value)}
-              className='rounded-md'
-              type='text'
-              placeholder='Book Name'
-            />
-          </div>
-          <div className='p-3 text-center'>
-            <h6>Enter Writer Name</h6>
-            <input
-              value={writerName}
-              onChange={(e) => setWriterName(e.target.value)}
-              className='rounded-md'
-              type='text'
-              placeholder='Writer Name'
-            />
-          </div>
-          <div className='p-3 text-center'>
-            <h6>Enter Total Page Number </h6>
-            <input
-              value={pageNumber}
-              onChange={(e) => setPageNumber(e.target.value)}
-              className='rounded-md'
-              type='number'
-              placeholder='Page Number'
-            />
-          </div>
-          <div className='flex justify-center p-3'>
-            <form onSubmit={handleSubmit}>
-              <button className='bg-blue-500 py-3 px-6 rounded-full text-white'>
-                Submit
-              </button>
-            </form>
-          </div>
-          <div className='p-3 text-center  text-white'>
-            <h3>{info}</h3>
-          </div>
-        </div>
-      </div>
-      <div>
-        <Booklist items={list} removeSpecificBook={removeSpecificBook} />
-      </div>
-      {list.length > 1 && (
-        <div className='flex justify-center my-5'>
-          <button
-            onClick={removeAllBooks}
-            className='px-8 py-4 bg-red-500 rounded-full text-white'
-          >
-            Remove All
-          </button>
-        </div>
-      )}
-    </div>
-  )
-}
+      <CreateBook
+        bookName={bookName}
+        writerName={writerName}
+        pageNumber={pageNumber}
+        handleSubmit={handleSubmit}
+        info={info}
+        setBookName={setBookName}
+        setWriterName={setWriterName}
+        setPageNumber={setPageNumber}
+      />
 
-// Function for rendering book list
-const Booklist = ({ items, removeSpecificBook }) => {
-  return (
-    <div className='container mx-auto'>
-      <div className='mt-20 flex flex-wrap items-center justify-center'>
-        {items.map((item) => {
-          return (
-            <div key={item.id} className='p-2 m-2 bg-yellow-100 w-1/4'>
-              <div className='p-1'>
-                <h5 className='font-semibold'>Book Name</h5>
-                <h3>{item.title}</h3>
-              </div>
-              <div className='p-1'>
-                <h5 className='font-semibold'>Writer Name</h5>
-                <h3>{item.writer}</h3>
-              </div>
-              <div className='p-1'>
-                <h5 className='font-semibold'>Total Page</h5>
-                <h3>{item.pageCount}</h3>
-              </div>
-              <div className='flex justify-end'>
-                <button
-                  onClick={() => removeSpecificBook(item.id)}
-                  className='px-4 py-2 bg-red-500 rounded-full text-white'
-                >
-                  Remove
-                </button>
-              </div>
-            </div>
-          )
-        })}
-      </div>
+      <BookList
+        items={list}
+        removeSpecificBook={removeSpecificBook}
+        removeAllBooks={removeAllBooks}
+      />
     </div>
   )
 }
