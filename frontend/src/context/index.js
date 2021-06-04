6ed4a001a0b20561fac5846bdd82813854cebbe4
Caner Kuncu <warlordsw@gmail.com>
2021-06-04 22:00:44 +0300

    export all methods for reducer and context hooks from index.js

---
+++ frontend/src/context/index.js
@@ -0,0 +1,12 @@
+import { loginUser, logout, registerUser, createBook } from './actions'
+import { AuthProvider, useAuthDispatch, useAuthState } from './context'
+
+export {
+  loginUser,
+  logout,
+  AuthProvider,
+  useAuthDispatch,
+  useAuthState,
+  registerUser,
+  createBook,
+}
