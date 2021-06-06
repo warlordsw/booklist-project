bc7c6da07a1ab95eba8582f933a4618a269de72e
Caner Kuncu <warlordsw@gmail.com>
2021-06-06 20:41:19 +0300

    add new routes for removeall and removespecific

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import { login, register } from '../controller/user_controller.js'
-import { getBooks, createBook } from '../controller/book_mongo_controller.js'
+import {
+  createBook,
+  removeAllBooks,
+  removeSpecificBook,
+} from '../controller/book_mongo_controller.js'
 import auth from '../middleware/auth.js'
 
 const router = express.Router()
@@ -9,5 +13,7 @@ router.post('/login', login)
 router.post('/register', register)
 //router.get('/books', getBooks)
 router.post('/books', createBook)
+router.post('/books/removeall', removeAllBooks)
+router.post('/books/removespecific', removeSpecificBook)
 
 export default router
