caecb7f96122485999625b9beeb92ea91691924a
Caner Kuncu <warlordsw@gmail.com>
2021-06-07 23:33:03 +0300

    add and modify auth middleware - it is active now

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,18 +5,20 @@ import jwt from 'jsonwebtoken'
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1]
-    let decodedData
-
-    if (token) {
-      decodedData = jwt.verify(token, 'test')
-      //sonra soru işaretsiz dene
-      req.userId = decodedData?.id
+    const token = req.body.token
+    //check for token
+    if (!token) {
+      res.status(401).json({ msg: 'Auth failed' })
     }
 
+    //verify token
+    const decoded = jwt.verify(token, 'test')
+
+    //Add user from payload
+    req.user = decoded
     next()
   } catch (error) {
-    console.log(error)
+    res.status(401).json({ msg: 'Token is not valid' })
   }
 }
 
